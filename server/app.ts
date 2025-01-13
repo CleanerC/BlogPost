05e420a57fc8447fb317fc31afd0b540f31acc5d
cleanerc <xjw031201@gmail.com>
2025-01-13 18:18:29 -0500

    update backend

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,14 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { blogsRoute } from './routes/blogs'
-import { serveStatic } from 'hono/serve-static'
+import { serveStatic } from 'hono/bun'
 
 const app = new Hono()
 app.use(logger())
 
 app.route("/api/blogs", blogsRoute)
+
+app.get('*', serveStatic({root: './frontend/dist'}))
+app.get('*', serveStatic({path: './frontend/dist/index.html'}))
  
 export default app
--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,8 @@
-import app from './app'
+import app from "./app";
 
-Bun.serve({
-    fetch: app.fetch    //everything will now be handled by hono
-  });
+const server = Bun.serve({
+  port: 3000,
+  fetch: app.fetch, //everything will now be handled by hono
+});
 
-console.log("Server Running...")
+console.log("Server Runnin on port: ", server.port);
